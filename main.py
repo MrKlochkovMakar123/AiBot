9b7a0241b9d03a34541c2149963f3a679ee5fb6d
MrKlochkovMakar123 <gaysdestroyer@gmail.com>
2024-11-12 02:41:10 +0300

    Final commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# import asyncio
+# import os
+# from aiogram import Bot, Dispatcher
+
+# await def main():
+#     # await async_main()
+
+#     bot = Bot(token=os.getenv('token'))
+#     dispatcher = Dispatcher()
+#     dispatcher.include_router(router)
+
+#     #await dispatcher.start_polling(bot)
+
+# if __name__ == '__main__':
+#     try:
+#         async.run(main())
+#     except KeyboardInterrupt:
+#         print("Бот выключен")
+
